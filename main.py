5e58b56907ccdf89da51d562197031683bd1fd4b
naskrosin <118050959+naskrosin@users.noreply.github.com>
2023-08-12 11:29:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,13 @@
 
 import random
 from sudoku_solver.grid import Grid
-from sudoku_solver.puzzles import hard#easy#, medium, hard
+from sudoku_solver.puzzles import easy #, medium, hard
 
 
-#game = Grid() # creating an empty Grid object
-#puzzle = random.choice(easy) # taking a random puzzle from easy list
-#
-#game.build(puzzle) # building the Grid object with puzzle numbers
-#game.draw() # print current status of the Grid
-#
-#game.solve() # solve and print the Grid puzzle
+game = Grid() # creating an empty Grid object
+puzzle = random.choice(easy) # taking a random puzzle from easy list
 
-for p in hard:
-    g = Grid()
-    g.build(p)
-    g.solve()
+game.build(puzzle) # building the Grid object with puzzle numbers
+game.draw() # print current status of the Grid
+
+game.solve() # solve and print the Grid puzzle
